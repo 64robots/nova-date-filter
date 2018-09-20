01039a988de503ade73df5bbf937b9b141b7d2e8
Javier Martinez <fjmartinez@disitec.es>
2018-09-20 22:16:58 +0200

    fix namespace

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -16,7 +16,7 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-row', __DIR__.'/../dist/js/date-filter.js');
+            Nova::script('nova-date-filter', __DIR__.'/../dist/js/date-filter.js');
         });
     }
 
