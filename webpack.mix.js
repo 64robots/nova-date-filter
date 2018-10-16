cb011379468631c87632f2d711a289c4403c482c
avrahamappel <aappel248@gmail.com>
2018-10-16 13:39:26 -0400

    new build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/date-filter.js', 'dist/js').webpackConfig({
+mix.js('resources/js', 'dist/js/custom-filters.js').webpackConfig({
   resolve: {
     symlinks: false
   }
